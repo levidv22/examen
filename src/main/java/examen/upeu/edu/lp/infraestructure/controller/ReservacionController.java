2a2d0b93474a9f25da71ec004076600f95f2ecbe
Lenovo <Lenovo@LeviDiaz>
2024-10-23 18:48:43 -0500

    Examen LP

--- src/main/java/examen/upeu/edu/lp/infraestructure/controller/ReservacionController.java
+++ src/main/java/examen/upeu/edu/lp/infraestructure/controller/ReservacionController.java
@@ -1,65 +1,51 @@
 package examen.upeu.edu.lp.infraestructure.controller;
 
-import examen.upeu.edu.lp.app.service.*;
-import examen.upeu.edu.lp.infraestructure.entity.*;
+import examen.upeu.edu.lp.app.service.RegistroService;
+import examen.upeu.edu.lp.infraestructure.entity.RegistroEntity;
 import java.util.*;
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/reservacion")
+@RequestMapping("/registro")
 public class ReservacionController {
 
-    private final ReservacionService reservacionService;
+    private final RegistroService registroService;
 
-    public ReservacionController(ReservacionService reservacionService) {
-        this.reservacionService = reservacionService;
+    public ReservacionController(RegistroService registroService) {
+        this.registroService = registroService;
     }
 
     @GetMapping("/show")
-    public String showReservas(Model model) {
-        List<ReservacionEntity> reservaciones = reservacionService.getReservas();
-        model.addAttribute("reservaciones", reservaciones);
-        Collections.reverse(reservaciones);
+    public String showRegistro(Model model) {
+        List<RegistroEntity> registro = registroService.getRegistro();
+        model.addAttribute("registro", registro);
         return "show";
     }
 
-    @GetMapping("/view/{id}")
-    public String getReservationById(@PathVariable Integer id, Model model) {
-        Optional<ReservacionEntity> reservacionOptional = reservacionService.getReservaById(id);
-
-        if (reservacionOptional.isPresent()) {
-            ReservacionEntity reservacion = reservacionOptional.get();
-            model.addAttribute("reservacion", reservacion);
-            return "view"; // Redirecciona a la vista "view_reservacion.html"
-        } else {
-            return "error/404"; // O redirige a una página de error si la reserva no existe
-        }
-    }
-
     @GetMapping("/create")
-    public String Reserva(Model model) {
-        model.addAttribute("reserva", new ReservacionEntity());
+    public String Registro(Model model) {
+        model.addAttribute("registro", new RegistroEntity());
         return "create";
     }
 
     @PostMapping("/create")
-    public String saveReserva(@ModelAttribute ReservacionEntity reserva) {
-        reservacionService.saveReserva(reserva);
-        return "redirect:/reservacion/show";
+    public String saveRegistro(@ModelAttribute RegistroEntity registroEntity) {
+        registroService.saveRegistro(registroEntity);
+        return "redirect:/registro/show";
     }
 
     @GetMapping("/edit/{id}")
     public String showEdit(@PathVariable Integer id, Model model) {
-        ReservacionEntity reserva = reservacionService.getReservaById(id).orElseThrow(() -> new IllegalArgumentException("ID no válido:" + id));
-        model.addAttribute("reserva", reserva);
+        RegistroEntity registro = registroService.getRegistroById(id).orElseThrow(() -> new IllegalArgumentException("ID no válido:" + id));
+        model.addAttribute("registro", registro);
         return "edit";
     }
 
     @GetMapping("/delete/{id}")
-    public String eliminarReserva(@PathVariable Integer id) {
-        reservacionService.deleteReserva(id);
-        return "redirect:/reservacion/show";
+    public String eliminarRegistro(@PathVariable Integer id) {
+        registroService.deleteRegistroById(id);
+        return "redirect:/registro/show";
     }
 }
